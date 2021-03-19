ced35e47305a42e4cd94749dc1ea824b108e537a
houyhourung <houyhourung88@gmail.com>
2021-03-19 16:24:37 -0400

    save changes before pull

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by HouyHour Ung on 3/19/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Podfile
+++ Podfile
@@ -7,6 +7,8 @@ target 'Instagram' do
 
   # Pods for Instagram
   pod 'Parse'
+  
+  pod 'AlamofireImage'
 
   target 'InstagramTests' do
     inherit! :search_paths
